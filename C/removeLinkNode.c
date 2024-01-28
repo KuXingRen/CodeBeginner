749ff20a93962d6103570b6937c7e30a4ebd0535
KuXingRen <quanying0@gmail.com>
2024-01-28 09:51:44 +0000

    Revert "添加释放链表空间的函数"
    This reverts commit c362b51cf4131eca7cebd0fa928745af13d8a7cf.

--- C/removeLinkNode.c
+++ C/removeLinkNode.c
@@ -18,23 +18,13 @@ int main() {
         int index = 0;
         ret = scanf("%d", &index);
         if (ret != 1 || index < 0) {
-            if (L == NULL) {
-                printf("删除失败,链表为空\n");
-            }
-            else {
-                printf("输入的不是一个合法的整数\n");
-            }
+            printf("输入的不是一个合法的整数\n");
             printf("\n请输入链表的长度\n");
             clearSTDIN();
             continue;
         }
-        ret = removeNodeByIndex(&L, index - 1);
-        if (!ret && L != NULL) {
-            printf("超出链表范围，删除失败\n");
-            continue;
-        }
+        removeNodeByIndex(index);
         showListNode(L);
-        freeLinkList(L);
         printf("请输入链表的长度\n");
     }
 }
