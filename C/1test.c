befa57d442e865de36623a0a39e1ae9d1989822f
KuXingRen <quanying0@gmail.com>
2024-03-14 05:40:47 +0000

    测试

--- C/1test.c
+++ C/1test.c
@@ -1,9 +1,10 @@
-#include"func.h"
+#include "func.h"
 
 int main() {
-    pNode L = NULL;
-    int i = 0;
-    structLinkListByRange(&L, 10, 5);
-    showListNode(L);
-    return 0;
+  pNode L = NULL;
+  int i = 0;
+  structLinkListByRange(&L, 10, 5);
+  showListNode(L);
+  freeLinkList(&L);
+  return 0;
 }
---
+++ C/Stack/linkStack.c
@@ -0,0 +1,12 @@
+// 栈的基本操作
+#include"func.h"
+
+void InitStack(pNode *L){
+    *L = NULL;
+}
+
+int main(){
+    pNode stack = NULL;
+    pNode pTop,pBottom;
+
+}
