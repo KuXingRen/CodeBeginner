42c389a2c0e267892b552697ba28c820cc888451
KuXingRen <quanying0@gmail.com>
2024-04-05 08:14:29 +0000

    添加系统编程依赖

--- C/.include/funclinux.h
+++ C/.include/funclinux.h
@@ -7,5 +7,7 @@
 #include<dirent.h>
 #include<string.h>
 #include<fcntl.h>
+#include<pwd.h>
+#include<grp.h>
 #define ARGS_CHECK(argc,num) {if(argc!=num){printf("error gras\n");return -1;}}
 #define ERROR_CHECK(ret,retval,func_name){if(ret==retval){perror(func_name);return -1;}}
