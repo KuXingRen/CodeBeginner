a3ac6500554421bed2bf140141c09f7697dfc89e
KuXingRen <quanying0@gmail.com>
2024-04-05 08:10:10 +0000

    完善

--- linux/deepDir.c
+++ linux/deepDir.c
@@ -1,12 +1,13 @@
 // 输入一个路径，以深度优先的方式遍历
 #include"funclinux.h"
 
-void printDir(const char *path, int width) {
+int printDir(const char *path, int width) {
     DIR *dir;
     char buf[1024];
     dir = opendir(path);
     if (NULL == dir) {
         perror("opendir");
+        return -1;
     }
     struct dirent *p;
     while (p = readdir(dir)) {
@@ -14,7 +15,9 @@ void printDir(const char *path, int width) {
         if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")) {
             continue;
         }
-        printf("%*s--%s\n", width, "", p->d_name);
+        // 实现类似tree的效果
+
+        printf("%*s%s\n", width, "", p->d_name);
         // 4 表示当前的文件类型时目录
         if (p->d_type == 4) {
             // 清空路径缓存
@@ -25,10 +28,32 @@ void printDir(const char *path, int width) {
     }
     // 关闭文件
     closedir(dir);
+    return 0;
+}
+
+//实现类似tree的效果
+int tabPrintDir(const char *path, int width)//这里实现了类似 tree 的效果
+{
+    DIR *pdir = opendir(path);
+    ERROR_CHECK(pdir, NULL, "opendir");
+    struct dirent *pdirent;
+    char buf[1024];
+    while ((pdirent = readdir(pdir))) {
+        if (strcmp(pdirent->d_name, ".") == 0 || strcmp(pdirent->d_name, "..") == 0) {
+            continue;
+        }
+        printf("%*s%s\n", width, "", pdirent->d_name);
+        sprintf(buf, "%s%s%s", path, "/", pdirent->d_name);
+        if (pdirent->d_type == 4) {
+            tabPrintDir(buf, width + 4);
+        }
+    }
+    closedir(pdir);
+    return 0;
 }
 
 int main() {
-    const char *path = ".";
-    printDir(path, 0);
+    const char *path = "..";
+    tabPrintDir(path, 0);
     return 0;
 }
