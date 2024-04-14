bfd20ba822c1ba7c1df616b821eb7f17c51f1e67
KuXingRen <quanying0@gmail.com>
2024-04-14 08:09:30 +0000

    半双工通信的管道实现

---
+++ linux/pipe/pipe_read.c
@@ -0,0 +1,11 @@
+// 该进程先写再读
+#include"funclinux.h"
+#include"func.h"
+int main(int argc, char *argv[]) {
+    ARGS_CHECK(argc, 2);
+    // 以只写的方式打开文件
+    int fd = open(argv[1], O_RDONLY);
+    ERROR_CHECK(fd, -1, "open");
+    printf("I am reader\n");
+    return 0;
+}
