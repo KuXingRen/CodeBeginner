c77c6662c42580b266e19fa29e55c73a2b0298f0
KuXingRen <quanying0@gmail.com>
2024-04-21 06:58:46 +0000

    fork与局部变量

--- linux/process/fork.c
+++ linux/process/fork.c
@@ -1,8 +1,16 @@
-// 使用fork创建子进程
+// 使用fork创建子进程，并打印两个进程的信息
 #include"func.h"
 #include"funclinux.h"
 int main() {
     pid_t pid;
-    printf("res\n");
+    pid = fork();
+    if (0 == pid) {
+        // 子进程的代码
+        printf("I am child,pid=%d,ppid=%d\n", getpid(), getppid());
+    }
+    else {
+        // 父进程的代码
+        printf("I am father,pid=%d,ppid=%d\n", getpid(), getppid());
+    }
     return 0;
 }
---
+++ linux/process/fork_lcoal.c
@@ -0,0 +1,20 @@
+// fork与局部变量共享情况(写时复制)
+#include"func.h"
+#include"funclinux.h"
+
+int main() {
+    pid_t pid;
+    int num = 10;
+    pid = fork();
+    if (0 == pid) {
+        printf("I am child,num=%d\n", num);
+    }
+    else {
+        printf("I am father,num=%d\n", num);
+        num = 5;
+        printf("I am father,num=%d\n", num);
+        sleep(1);
+        return 0;
+    }
+
+}
